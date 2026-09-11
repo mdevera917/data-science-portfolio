0e6c2ad0d238db6e7d524d7053431a8b9455813b
Mark De Vera <msdevera02@gmail.com>
2026-09-10 18:53:38 -0700

    Added learning question and another personalized message

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,6 +1,11 @@
 # My first Python project in GitHub
 def greet_user():
  name = input("Enter your name: ")
+ question = input("What are you looking forward to learning? ")
+ 
  print(f"Hello, {name}! Welcome to Data Science.")
+ print(f"That's great that you're looking forward to learning {question}!")
+ print("Good luck with your studies!")
+ 
 if __name__ == "__main__":
  greet_user()
