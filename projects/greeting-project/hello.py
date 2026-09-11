2c332f52d87df91bd8d28190a328570d6503fb83
Mark De Vera <msdevera02@gmail.com>
2026-09-10 18:04:02 -0700

    Delete projects/greeting-project directory

--- projects/greeting-project/hello.py
+++
@@ -1,6 +0,0 @@
-# My first Python project in GitHub
-def greet_user():
- name = input("Enter your name: ")
- print(f"Hello, {name}! Welcome to Data Science.")
-if __name__ == "__main__":
- greet_user()
